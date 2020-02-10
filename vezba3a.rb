80fb4f14ccc73f2fec2ca05fdfcf688b50307186
Nikola <60677366+nikolatriki@users.noreply.github.com>
2020-02-11 00:58:56 +0100

    Add files via upload

---
+++ vezba3a.rb
@@ -0,0 +1,29 @@
+# A line of text is printed about what is supposed to be done
+puts "I will now count my chickens:"
+# Type of chicken displayed and calculation
+puts "Hens", 25 + 30 / 6
+# Type of chicken displayed and calculation 
+puts "Roosters", 100 - 25 * 3 % 4
+# A line of text about what is supposed to be counted next
+puts "Now I will count the eggs:"
+# A mathematical operation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Prints statement and corresponding answer
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Shows result of calculation
+puts 3 + 2 < 5 - 7
+# Displays question and a result of calculation
+puts "What is 3 + 2?", 3 + 2
+# Displays question and a result of calculation
+puts "What is 5 - 7?", 5 - 7
+# A line of text to display
+puts "Oh, that's why it's false."
+# Another line od text to display
+puts "How about some more."
+# Prints question and the resulting true/ false statement from a calculation
+puts "Is it greater?", 5 > -2
+# Prints question and the resulting true/ false statement from a calculation
+puts "Is it greater or equal?", 5 >= -2
+# Prints question and the resulting true/ false statement from a calculation
+puts "Is it less or equal?", 5 <= -2
+# end of code
